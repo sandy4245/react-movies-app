b89f68b296b3477759bf49c86347417903e35c1e
sandy4245 <sandeep.padhi@gmail.com>
2021-06-13 21:08:06 +0530

    Added ‘Home’ component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './screens/home/Home';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div></div>,
+  document.getElementById('root')
+);
+ReactDOM.render(
+  <Home/>,
   document.getElementById('root')
 );
 
