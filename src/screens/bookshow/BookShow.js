0ec9fa3dc1169f92732cc080d15a9f3d66142697
sandy4245 <sandeep.padhi@gmail.com>
2021-06-26 18:04:08 +0530

    Added unit price, total price and book show button on book show page

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -16,6 +16,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 
 
 class BookShow extends Component {
@@ -24,16 +25,37 @@ class BookShow extends Component {
         super();
 
         this.state = {
-
-
+            location: "",
+            language: "",
+            showDate: "",
+            showTime: "",
+            tickets: 0,
+            unitPrice: 500,
+            availableTickets: 20
         }
     }
+
 
     backtodetailsHandler = () => {
         ReactDOM.render(<Details movieId={this.props.movieId} />, document.getElementById('root'));
     }
     locationChangeHandler = event => {
         this.setState ({location:event.target.value});
+    }
+    languageChangeHandler = event => {
+        this.setState({ language: event.target.value });
+    }
+
+    showDateChangeHandler = event => {
+        this.setState({ showDate: event.target.value });
+    }
+
+    showTimeChangeHandler = event => {
+        this.setState({ showTime: event.target.value });
+    }
+
+    ticketsChangeHandler = event => {
+        this.setState ({tickets: event.target.value});
     }
     render() {
         return (
@@ -61,6 +83,62 @@ class BookShow extends Component {
                                 ))}
                                 </Select>
                             </FormControl>
+                            <br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="language">Choose Language:</InputLabel>
+                                <Select
+                                    value={this.state.language}
+                                    onChange={this.languageChangeHandler}
+                                >
+                                    {language.map(lang => (
+                                        <MenuItem key={"lang" + lang.id} value={lang.language}>
+                                            {lang.language}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="showDate">Choose Show Date:</InputLabel>
+                                <Select
+                                    value={this.state.showDate}
+                                    onChange={this.showDateChangeHandler}
+                                >
+                                    {showDate.map(sd => (
+                                        <MenuItem key={"showDate" + sd.id} value={sd.showDate}>
+                                            {sd.showDate}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="showTime">Choose Show Time:</InputLabel>
+                                <Select
+                                    value={this.state.showTime}
+                                    onChange={this.showTimeChangeHandler}
+                                >
+                                    {showTime.map(st => (
+                                        <MenuItem key={"showTime" + st.id} value={st.showTime}>
+                                            {st.showTime}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <FormControl required className="formControl">
+                            <InputLabel htmlFor="tickets">Tickets: {this.state.availableTickets} available</InputLabel>
+                            <Input id="tickets" value={this.state.tickets !== 0 ?
+                            this.state.tickets: ""} onChange=
+                            {this.ticketsChangeHandler}/>
+                                </FormControl><br/><br/>
+                                <Typography>
+                                    Unit Price: Rs. {this.state.unitPrice}
+                                </Typography><br/>
+                                <Typography>
+                                    Total Price: Rs. {this.state.unitPrice * this.state.tickets}
+                                </Typography><br/> <br/>
+                                <Button variant="contained" color="primary" onClick={this.bookShowButtonHandler}>
+                                Book Show</Button>
                         </CardContent>
                     </Card> 
 
